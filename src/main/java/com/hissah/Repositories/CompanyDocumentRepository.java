bcf65a43d4740a80d8c40557160f8858c6cb6944
al-zaharaa <shahdaljabri33@gmail.com>
2026-07-27 16:20:41 +0400

    Add CompanyDocumentRepository interface for document data operations

---
+++ src/main/java/com/hissah/Repositories/CompanyDocumentRepository.java
@@ -0,0 +1,17 @@
+package com.hissah.Repositories;
+
+
+
+import com.hissah.Entities.CompanyDocument;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CompanyDocumentRepository extends JpaRepository<CompanyDocument, Long> {
+
+
+    List<CompanyDocument> findByCompanyId(Long companyId);
+
+}
