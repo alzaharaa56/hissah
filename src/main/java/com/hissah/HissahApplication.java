4271722e213a894bcc6145eedce45f07d0d21d0d
al-zaharaa <shahdaljabri33@gmail.com>
2026-07-27 12:46:54 +0400

    chore(project): initialize Hissah Spring Boot project

---
+++ src/main/java/com/hissah/HissahApplication.java
@@ -0,0 +1,13 @@
+package com.hissah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HissahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HissahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hissah/HissahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hissah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HissahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
