81ac02a59deed690ae0222c001f1b1a7f790857c
al-zaharaa <shahdaljabri33@gmail.com>
2026-07-28 08:36:31 +0400

    Update CompanyDocumentRepository with custom @Query annotation

--- src/main/java/com/hissah/Repositories/CompanyDocumentRepository.java
+++ src/main/java/com/hissah/Repositories/CompanyDocumentRepository.java
@@ -1,9 +1,9 @@
 package com.hissah.Repositories;
 
-
-
 import com.hissah.Entities.CompanyDocument;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +12,7 @@ import java.util.List;
 public interface CompanyDocumentRepository extends JpaRepository<CompanyDocument, Long> {
 
 
-    List<CompanyDocument> findByCompanyId(Long companyId);
+    @Query("SELECT cd FROM CompanyDocument cd WHERE cd.companyId = :companyId")
+    List<CompanyDocument> findDocumentsByCompanyId(@Param("companyId") Long companyId);
 
 }
