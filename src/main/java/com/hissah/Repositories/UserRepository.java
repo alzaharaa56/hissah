92f54607c8894b42c74788272cf376fbee1f5a2f
al-zaharaa <shahdaljabri33@gmail.com>
2026-07-28 08:39:51 +0400

    Update UserRepository with custom @Query annotation

--- src/main/java/com/hissah/Repositories/UserRepository.java
+++ src/main/java/com/hissah/Repositories/UserRepository.java
@@ -1,7 +1,10 @@
 package com.hissah.Repositories;
 
+
 import com.hissah.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,6 +13,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
-    Optional<User> findByEmail(String email);
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    Optional<User> findUserByEmail(@Param("email") String email);
 
 }
