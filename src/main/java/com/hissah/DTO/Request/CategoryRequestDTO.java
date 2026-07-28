92c9d4409bef6833d45d34fbdf23c8f13430dbc0
al-zaharaa <shahdaljabri33@gmail.com>
2026-07-28 10:04:22 +0400

    Add CategoryRequestDTO class for category creation and updates

---
+++ src/main/java/com/hissah/DTO/Request/CategoryRequestDTO.java
@@ -0,0 +1,26 @@
+package com.hissah.DTO.Request;
+
+import com.hissah.Entities.Category;
+import lombok.Data;
+
+@Data
+public class CategoryRequestDTO {
+    private String name;
+    private Long parentCategoryId;
+    private Boolean active;
+
+    public Category toEntity() {
+        Category category = new Category();
+        category.setName(this.name);
+        category.setParentCategoryId(this.parentCategoryId);
+        category.setActive(this.active != null ? this.active : true);
+        return category;
+    }
+
+    public void updateEntity(Category category) {
+        if (category == null) return;
+        if (this.name != null) category.setName(this.name);
+        if (this.parentCategoryId != null) category.setParentCategoryId(this.parentCategoryId);
+        if (this.active != null) category.setActive(this.active);
+    }
+}
