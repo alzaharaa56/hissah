1945995501894db77422477075723c9b23922e0d
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-07-29 10:07:42 +0400

    feat(repositories/specifications): dynamic opportunity search filters.

--- src/main/java/com/hissah/Repositories/Specifications/WorkPackageSpecification.java
+++ src/main/java/com/hissah/Repositories/Specifications/WorkPackageSpecification.java
@@ -3,7 +3,7 @@
  */
 package com.hissah.Repositories.Specifications;
 
-import com.hissah.DTOs.Requests.WorkPackageSearchRequestDTO;
+import com.hissah.DTO.Requests.WorkPackageSearchRequestDTO;
 import com.hissah.entities.WorkPackage;
 import jakarta.persistence.criteria.JoinType;
 import jakarta.persistence.criteria.Predicate;
