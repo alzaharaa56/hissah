945a1c33a30c5c0bcf117620835ed001a51006b6
al-zaharaa <shahdaljabri33@gmail.com>
2026-07-29 12:13:09 +0400

    update companyCategporyRepository

--- src/main/java/com/hissah/Repositories/CompanyCategoryRepository.java
+++ src/main/java/com/hissah/Repositories/CompanyCategoryRepository.java
@@ -1,6 +1,6 @@
 package com.hissah.Repositories;
 
-
+import com.hissah.Entities.Category;
 import com.hissah.Entities.CompanyCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,10 +12,10 @@ import java.util.List;
 @Repository
 public interface CompanyCategoryRepository extends JpaRepository<CompanyCategory, Long> {
 
+
     @Query("SELECT cc FROM CompanyCategory cc WHERE cc.companyId = :companyId")
     List<CompanyCategory> findCategoriesByCompanyId(@Param("companyId") Long companyId);
 
     @Query("SELECT cc FROM CompanyCategory cc WHERE cc.categoryId = :categoryId")
     List<CompanyCategory> findCompaniesByCategoryId(@Param("categoryId") Long categoryId);
-
 }
