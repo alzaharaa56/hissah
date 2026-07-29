4c556dfdc8a5007b229142cb535c20c8dd7e3027
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-07-29 09:51:09 +0400

    feat(dto): add reusable optional workflow reason request

---
+++ src/main/java/com/hissah/DTO/Requests/ReasonRequestDTO.java
@@ -0,0 +1,19 @@
+package com.hissah.DTO.Requests;
+
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReasonRequestDTO {
+
+    @Size(max = 1000, message = "Reason must not exceed 1000 characters.")
+    private String reason;
+}
