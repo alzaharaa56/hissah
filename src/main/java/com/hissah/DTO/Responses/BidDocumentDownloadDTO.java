a4f9e36fb3809b43868d189f27125f1ebbf62d99
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-07-29 09:51:45 +0400

    feat(dto): add protected bid document download response

---
+++ src/main/java/com/hissah/DTO/Responses/BidDocumentDownloadDTO.java
@@ -0,0 +1,15 @@
+package com.hissah.DTO.Responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.nio.file.Path;
+
+@Getter
+@AllArgsConstructor
+public class BidDocumentDownloadDTO {
+    private final Path path;
+    private final String fileName;
+    private final String contentType;
+    private final Long fileSize;
+}
