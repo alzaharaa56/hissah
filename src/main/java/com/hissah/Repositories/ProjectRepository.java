ba7b2fc6560488356a193cb420076dd6a8ec7a71
al-zaharaa <shahdaljabri33@gmail.com>
2026-07-30 08:50:06 +0400

    Update ProjectRepository with custom queries and methods

--- src/main/java/com/hissah/Repositories/ProjectRepository.java
+++ src/main/java/com/hissah/Repositories/ProjectRepository.java
@@ -1,6 +1,5 @@
 package com.hissah.Repositories;
 
-
 import com.hissah.Entities.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,13 +7,21 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
+    Optional<Project> findByIdAndActiveTrue(Long id);
+
+    List<Project> findByContractorCompanyIdAndActiveTrueOrderByCreatedAtDesc(Long contractorCompanyId);
+
+    List<Project> findTop5ByContractorCompanyIdAndActiveTrueOrderByCreatedAtDesc(Long contractorCompanyId);
+
+    List<Project> findTop5ByActiveTrueOrderByCreatedAtDesc();
+
+    long countByContractorCompanyIdAndActiveTrue(Long contractorCompanyId);
 
     @Query("SELECT p FROM Project p WHERE p.contractorCompanyId = :contractorCompanyId")
     List<Project> findProjectsByContractorCompanyId(@Param("contractorCompanyId") Long contractorCompanyId);
-
 }
-
