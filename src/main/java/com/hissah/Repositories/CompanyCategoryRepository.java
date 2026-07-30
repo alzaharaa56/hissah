356e8fdfb9a488df5bb5231470f79a83f1e1357e
al-zaharaa <shahdaljabri33@gmail.com>
2026-07-30 08:47:37 +0400

    Update CompanyCategoryRepository

--- src/main/java/com/hissah/Repositories/CompanyCategoryRepository.java
+++ src/main/java/com/hissah/Repositories/CompanyCategoryRepository.java
@@ -1,6 +1,6 @@
 package com.hissah.Repositories;
 
-import com.hissah.Entities.Category;
+
 import com.hissah.Entities.CompanyCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,10 +8,20 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CompanyCategoryRepository extends JpaRepository<CompanyCategory, Long> {
 
+    Optional<CompanyCategory> findByIdAndActiveTrue(Long id);
+
+    List<CompanyCategory> findByCompanyId(Long companyId);
+
+    List<CompanyCategory> findByCategoryId(Long categoryId);
+
+    boolean existsByCompanyIdAndCategoryId(Long companyId, Long categoryId);
+
+    void deleteByCompanyIdAndCategoryId(Long companyId, Long categoryId);
 
     @Query("SELECT cc FROM CompanyCategory cc WHERE cc.companyId = :companyId")
     List<CompanyCategory> findCategoriesByCompanyId(@Param("companyId") Long companyId);
