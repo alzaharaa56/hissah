50a7b399e0f06963081cf08517f407687b98759e
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-08-04 09:06:45 +0400

    fix: update REST authentication error response

--- src/main/java/com/hissah/Security/RestAuthenticationEntryPoint.java
+++ src/main/java/com/hissah/Security/RestAuthenticationEntryPoint.java
@@ -1,6 +1,6 @@
 package com.hissah.Security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.json.JsonMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
@@ -18,9 +18,9 @@ import java.util.Map;
 @Component
 public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-    private final ObjectMapper objectMapper;
+    private final JsonMapper objectMapper;
 
-    public RestAuthenticationEntryPoint(ObjectMapper objectMapper) {
+    public RestAuthenticationEntryPoint(JsonMapper objectMapper) {
         this.objectMapper = objectMapper;
     }
 
