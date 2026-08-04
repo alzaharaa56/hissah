5560ae7a970b9afa41dc15f6db46d6347861197c
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-08-04 08:58:17 +0400

    fix: update category response DTO

--- src/main/java/com/hissah/DTO/Response/CategoryResponseDTO.java
+++ src/main/java/com/hissah/DTO/Response/CategoryResponseDTO.java
@@ -3,12 +3,16 @@ package com.hissah.DTO.Response;
 import com.hissah.Entities.Category;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class CategoryResponseDTO {
     private Long id;
     private String name;
     private Long parentCategoryId;
     private Boolean active;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     public static CategoryResponseDTO fromEntity(Category category) {
         if (category == null) return null;
@@ -17,6 +21,8 @@ public class CategoryResponseDTO {
         response.setName(category.getName());
         response.setParentCategoryId(category.getParentCategoryId());
         response.setActive(category.getActive());
+        response.setCreatedAt(category.getCreatedAt());
+        response.setUpdatedAt(category.getUpdatedAt());
         return response;
     }
 }
