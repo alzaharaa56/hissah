3847585d14cf0d19df3825e5af3800cc7c7bf9c0
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-08-04 08:55:35 +0400

    fix: update category request DTO

--- src/main/java/com/hissah/DTO/Request/CategoryRequestDTO.java
+++ src/main/java/com/hissah/DTO/Request/CategoryRequestDTO.java
@@ -6,21 +6,37 @@ import lombok.Data;
 @Data
 public class CategoryRequestDTO {
     private String name;
+    private String description;
     private Long parentCategoryId;
     private Boolean active;
 
     public Category toEntity() {
         Category category = new Category();
         category.setName(this.name);
+        category.setDescription(this.description);
         category.setParentCategoryId(this.parentCategoryId);
         category.setActive(this.active != null ? this.active : true);
         return category;
     }
 
     public void updateEntity(Category category) {
-        if (category == null) return;
-        if (this.name != null) category.setName(this.name);
-        if (this.parentCategoryId != null) category.setParentCategoryId(this.parentCategoryId);
-        if (this.active != null) category.setActive(this.active);
+        if (category == null) {  return;
+        }
+
+        if (this.name != null) {
+            category.setName(this.name);
+        }
+
+        if (this.description != null) {
+            category.setDescription(this.description);
+        }
+
+        if (this.parentCategoryId != null) {
+            category.setParentCategoryId(this.parentCategoryId);
+        }
+
+        if (this.active != null) {
+            category.setActive(this.active);
+        }
     }
 }
