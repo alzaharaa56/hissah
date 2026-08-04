93635547ec18e39d8bc9531dedecf8ec7ccd35fb
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-08-04 09:03:58 +0400

    fix: load authenticated user company details

--- src/main/java/com/hissah/Security/CustomUserDetailsService.java
+++ src/main/java/com/hissah/Security/CustomUserDetailsService.java
@@ -1,6 +1,8 @@
 package com.hissah.Security;
 
+import com.hissah.Entities.Company;
 import com.hissah.Entities.User;
+import com.hissah.Repositories.CompanyRepository;
 import com.hissah.Repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,16 +12,33 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailsService implements UserDetailsService {
+public class CustomUserDetailsService
+        implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final CompanyRepository companyRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(
+            String email
+    ) throws UsernameNotFoundException {
 
         User user = userRepository.findUserByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "User not found with email: "
+                                        + email
+                        )
+                );
 
-        return new CustomUserPrincipal(user, null);
+        Long companyId = companyRepository
+                .findByUserId(user.getId())
+                .map(Company::getId)
+                .orElse(null);
+
+        return new CustomUserPrincipal(
+                user,
+                companyId
+        );
     }
 }
